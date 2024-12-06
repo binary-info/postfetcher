187f63630331b5037ca6c321e4a23734689b5af5
Raj Undaviya <undaviyaraj2000@gmail.com>
2024-12-06 18:40:07 +0530

    Change port

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ async def root():
 
 if __name__=="__main__":
     print(" --- Welcome to App ---")
-    uvicorn.run(app, host="127.0.0.1", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8501)
